d2d7e9307c7b7fb1c39bd52971be6a5058b7b79d
ABDULKREM AL SAIF <abdulkrem.alsaif@gmail.com>
2024-04-24 04:10:43 +0300

    Add More Info

--- js/move.js
+++ js/move.js
@@ -1,3 +1,6 @@
 function programmingPart() {
     document.getElementById('programmingLanguage').scrollIntoView();
 }
+function goToSatr() {
+    window.open("https://profile.satr.codes/abdulkrem_al_saif/public/overview", "_blank");
+}
