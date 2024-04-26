3bb4e5e615d9a9e05e8dacdd668e7bfcebc753f6
ABDULKREM AL SAIF <abdulkrem.alsaif@gmail.com>
2024-04-26 04:01:52 +0300

    Add More Link

--- js/move.js
+++ js/move.js
@@ -4,3 +4,33 @@ function programmingPart() {
 function goToSatr() {
     window.open("https://profile.satr.codes/abdulkrem_al_saif/public/overview", "_blank");
 }
+function java() {
+    window.open("https://satr.codes/courses/DcFViVITbo/view", "_blank");
+}
+function c_sharp() {
+    window.open("https://satr.codes/courses/uxpcCprKpV/view", "_blank");
+}
+function python() {
+    window.open("https://satr.codes/courses/rRUQyVDkwQ/view", "_blank");
+}
+function rubyAndSinatra() {
+    window.open("https://www.rwaq.org/courses/introduction-to-web-development", "_blank");
+}
+function django() {
+    window.open("https://satr.codes/courses/FymypsKhJJ/view", "_blank");
+}
+function html5() {
+    window.open("https://satr.codes/courses/CATspNvVjT/view", "_blank");
+}
+function css3() {
+    window.open("https://satr.codes/courses/hNUwyBUKmF/view", "_blank");
+}
+function javascript() {
+    window.open("https://satr.codes/courses/MxhtosVATL/view", "_blank");
+}
+function sql() {
+    window.open("https://satr.codes/courses/FtkmhtJpQW/view", "_blank");
+}
+function json() {
+    window.open("https://satr.codes/courses/JBkVaduXeJ/view", "_blank");
+}
